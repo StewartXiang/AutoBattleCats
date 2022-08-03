75055520060a68bf9c8eb3e0cc730584edf32741
StewartXiang <s.t.xiang@outlook.com>
2022-08-03 23:01:28 +0800

    refacting check img

--- task.js
+++ task.js
@@ -1,9 +1,18 @@
+const acc = require("accessibility")
+const img = require("image")
 const TaskStatus = {
     "prepare": 1, 
     "restore": 2,
     "battle": 3, 
     "end": 4,
     "term": 5,
+}
+const TplAttr = {
+    "img": 0, 
+    "x": 1, 
+    "y": 2, 
+    "width": 3, 
+    "height": 4
 }
 
 class Task{
@@ -28,5 +37,55 @@ class Task{
     term(){
         this.status = TaskStatus.term
     }
+}
 
+
+class BasicStep {
+    constructor(task){
+        this.task = task
+    }
+    exec(){
+        pass
+    }
+}
+
+class StepPrepare extends BasicStep{
+    constructor(task){
+        super(task)
+
+    }
+    exec(){
+        this.task.step = StepRestore(this.task) 
+    }
+}
+
+class StepRestore extends BasicStep{
+    exec(){
+    }
+}
+
+class StepBattle extends BasicStep{
+
+}
+
+class StepEnd extends BasicStep{
+
+}
+
+class StepTerm extends BasicStep{
+
+}
+
+async function check(imgName){
+    template = globalThis.imgInfo[imgName]
+    screen = await acc.takeScreenShot()
+    result = img.findImageInRegionSync(
+        screen, 
+        template[TplAttr.img], 
+        template[TplAttr.x], 
+        template[TplAttr.y], 
+        template[TplAttr.height], 
+        template[TplAttr.width]
+    )
+    return result
 }
